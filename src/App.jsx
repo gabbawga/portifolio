16491660e92cbf9fbec6ce936aea00126ae0c522
˜Gabriel <gabrielferreiraa05@gmail.com>
2025-05-21 22:42:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>Wellcome to my Portfolio</h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
+
