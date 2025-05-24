cd15036460d2540e818d316e02bbd148752fddf2
˜Gabriel <gabrielferreiraa05@gmail.com>
2025-05-24 16:09:05 -0300

    criando sections do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
+import Hero from "./sections/Hero.jsx";
 
 const App = () => {
   return (
-    <main>
-      <div>
-        <h1>Wellcome to my Portfolio</h1>
-      </div>
-    </main>
+   <>
+       <Hero />
+   </>
   );
 };
 
